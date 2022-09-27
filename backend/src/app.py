b4258c18beff23fa433ea855da35eaff769a4388
Richard Johansson <richard@queenslab.se>
2022-09-27 13:30:15 +0200

    Initial commit

---
+++ backend/src/app.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "HELLO WORLD!"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
