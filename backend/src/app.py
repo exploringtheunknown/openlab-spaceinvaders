8b399b026a74f7b958ef9d3602168584927f5a3c
elizaadolfsson <eliza@queenslab.se>
2022-10-11 18:04:29 +0200

    style: Successful pre-commit test
    Black and Flake8 did their job

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,10 +3,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return "HELLO WORLD!"
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
+
+# HEHJ
+#
+##
