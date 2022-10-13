b6cc302cc0ce1a6bbe3f12ef39aaac6c13fe907f
Richard Johansson <richard@queenslab.se>
2022-10-13 12:44:32 +0200

    feat: add Light model & getLight service function

--- backend/src/lights/models.py
+++ backend/src/lights/models.py
@@ -1,7 +1,9 @@
 from adafruit_blinka.microcontroller.generic_micropython import Pin
-from neopixel import GRB, NeoPixel, Color
-from pydantic import BaseModel 
-from typing import Tuple, List
+from neopixel import GRB, NeoPixel
+from pydantic import BaseModel
+from typing import Tuple, List, Literal
+
+LIGHT_TYPES = Literal["squre_light", "rectangle_light"]
 
 
 class Light(BaseModel):
@@ -9,16 +11,18 @@ class Light(BaseModel):
     num_pixels: int
     height_pixels: int
     width_pixels: int
- 
+
     def __init__(self, pin_id: int, height_pixels: int, width_pixels: int):
         num_pixels = height_pixels * width_pixels
 
-        self.neopixel = NeoPixel(Pin(pin_id), num_pixels, brightness=0.2, auto_write=False, pixel_order=GRB)
+        self.neopixel = NeoPixel(
+            Pin(pin_id), num_pixels, brightness=0.2, auto_write=False, pixel_order=GRB
+        )
         self.height_pixels = height_pixels
         self.width_pixels = width_pixels
         self.num_pixels = num_pixels
-    
-    def loading(self, color: Color):
+
+    def loading(self, color: Tuple[int]):
         self.neopixel.fill(color)
         self.neopixel.show()
 
@@ -28,11 +32,11 @@ class Light(BaseModel):
 
     def fill_by_index(self, indexes: List[dict]):
         for pixel in range(self.num_pixels):
+            # Replace with logic when PostModel exists
             existing_pixel = indexes[0]
-
             if existing_pixel is not None:
-                self.neopixel[pixel] = existing_pixel['color']
+                self.neopixel[pixel] = existing_pixel["color"]
             else:
-                self.neopixel[pixel] = (0,0,0)
+                self.neopixel[pixel] = (0, 0, 0)
 
             self.neopixel.show()
--- backend/src/lights/service.py
+++ backend/src/lights/service.py
@@ -1,11 +1,12 @@
 from fastapi import HTTPException
-from .models import Light
+from .models import Light, LIGHT_TYPES
 
-def getLight(light_string: str) -> Light:
+
+def getLight(light_string: LIGHT_TYPES) -> Light:
     match light_string:
-        case 'square_light':
+        case "square_light":
             return Light(pin_id=21, width_pixels=16, height_pixels=16)
-        case 'rectangular_light':
+        case "rectangular_light":
             return Light(pin_id=18, width_pixels=32, height_pixels=8)
         case _:
-            raise HTTPException(status_code=404, detail="Could not find suitable light") 
+            raise HTTPException(status_code=404, detail="Could not find suitable light")
