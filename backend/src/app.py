7355da9abf5ff871e7fa5d173096c80422d009f4
elizaadolfsson <eliza@queenslab.se>
2022-11-10 18:28:12 +0100

    feat(models.py): added rainbow funcs

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .lights.routes import router as light_router
 
 app = FastAPI()
 
 app.include_router(light_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
