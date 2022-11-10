3d1a35d83e3c21ad1a802ab687c076a044001e4f
Richard Johansson <richard@queenslab.se>
2022-11-10 20:22:11 +0100

    fix: if else instead of match

--- backend/src/lights/service.py
+++ backend/src/lights/service.py
@@ -3,10 +3,9 @@ from .models import Light, LIGHT_TYPES
 
 
 def getLight(light_type: LIGHT_TYPES) -> Light:
-    match light_type:
-        case "square_light":
-            return Light(pin_id=21, width_pixels=16, height_pixels=16)
-        case "rectangle_light":
-            return Light(pin_id=21, width_pixels=32, height_pixels=8)
-        case _:
-            raise HTTPException(status_code=404, detail="Could not find suitable light")
+    if light_type == "square_light":
+        return Light(pin_id=21, width_pixels=16, height_pixels=16)
+    else if "rectangle_light":
+        return Light(pin_id=21, width_pixels=32, height_pixels=8)
+    else:
+        raise HTTPException(status_code=404, detail="Could not find suitable light")
